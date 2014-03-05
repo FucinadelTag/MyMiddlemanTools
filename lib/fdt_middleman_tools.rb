c9e8ce8c7c0f676383da5480b202f454a44e9cd4
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2014-03-05 08:35:28 +0100

    Aggiunto helper per lista delle risorge in middleman

--- lib/fdt_middleman_tools.rb
+++ lib/fdt_middleman_tools.rb
@@ -1,5 +1,7 @@
-class FdtMiddlemanTools
-  def self.hi
-    puts "Hello world!"
+module FdtMiddlemanTools
+
+  def find_resource_by_path (path = '*', sortField = 'title')
+    sitemap.resources.select {|p| p.path.match(/#{path}/) }.sort{|x,y| x.data."#{sortField}" <=> y.data.title}
   end
+
 end
