5bd7faec3e54f6709b6dedd4c04dd185758a0ad1
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2014-03-05 08:46:36 +0100

    Modifica alla classe

--- lib/fdt_middleman_tools.rb
+++ lib/fdt_middleman_tools.rb
@@ -1,7 +1,7 @@
 module FdtMiddlemanTools
 
   def find_resource_by_path (path = '*', sortField = 'title')
-    sitemap.resources.select {|p| p.path.match(/#{path}/) }.sort{|x,y| x.data."#{sortField}" <=> y.data.title}
+    sitemap.resources.select {|p| p.path.match(/#{path}/) }.sort{|x,y| x.data.sortField <=> y.data.sortField}
   end
 
 end
