864fd7d244351bf95cbe1c5f219e67460e9b0062
Lorenzo Caldara <lorenzo.caldara@gmail.com>
2014-03-05 09:02:34 +0100

    Risolto bug nome variabile

--- lib/fdt_middleman_tools.rb
+++ lib/fdt_middleman_tools.rb
@@ -1,7 +1,7 @@
 module FdtMiddlemanTools
 
   def find_resource_by_path (path = '*', sortField = 'title')
-    sitemap.resources.select {|p| p.path.match(/#{path}/) }.sort{|x,y| x.data.sortField <=> y.data.sortField}
+    sitemap.resources.select {|p| p.path.match(/#{path}/) }.sort{|x,y| x.data.instance_eval(sortField) <=> y.data.instance_eval(sortField)}
   end
 
 end
